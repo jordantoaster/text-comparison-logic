8f44ad8ce5fb16583576f672d788706f97c0d354
jordantoaster <jordanmcdonaldmain@gmail.com>
2019-10-12 15:48:49 +0100

    v2 fail

--- diff_worker.py
+++ diff_worker.py
@@ -16,23 +16,37 @@ doc_after_deletion = 'this is a matching string -  - it is a simple example'
 doc_before_both = 'this is a *delete* piece of sample text'
 doc_after_both = 'this is a piece of sample text *add*'
 
+# 5 Addition & Deletion - DOES NOT WORK FOR THIS ONE
+doc_before_both = 'this is a piece of sample text *delete*'
+doc_after_both = 'this is a *add* piece of sample text'
+
+# 6 Longer B, more text, random add and deletes - Get stuck in loop
+doc_before_big = 'this is a piece of sample text, this is a piece of sample text, this is a piece of sample text, this is a piece of sample text, this is a piece of sample text, this is a piece of sample text'
+doc_after_big = 'this is a  of sample text, this is a piece Hello sample text, this is a piece of sample test, this is a piece of sample text, this is a piece of  text, this is a piece of sample text, this is a piece of sample text, this is a piece of sample text'
+
 # Data Structure for Changes
 class ChChChChanges():
 
     def __init__(self):     
         self.matching_text = ""
-        self.changed_text = []
+        self.changes = []
+        self.changed_text = ""
 
     def append_matching_chunk(self, text_chunk :str):
         self.matching_text += text_chunk
 
-    def append_change_chunk(self, text_chunk :str):
-        self.changed_text.append(text_chunk)
+    def append_change_chunk(self, change_information :dict):
+        self.changes.append(change_information)
+
+    def append_change_text(self, text_chunk :str):
+        self.changed_text += text_chunk
 
     def print_collected_data(self):
         print(self.matching_text)
         print('----BREAK----')
         print(self.changed_text)
+        print('----BREAK----')
+        print(self.changes)
 
 
 # Get shortest input Length
@@ -52,13 +66,16 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
 
     idx_short = 0
     idx_long = 0
+    focused_doc = ''
 
     if is_before_longest:
         long_doc = doc_before
         short_doc = doc_after
+        focused_doc = 'before'
     else:
         long_doc = doc_after
         short_doc = doc_before 
+        focused_doc = 'after'
 
     while(idx_short < longest_length or idx_long < longest_length):
 
@@ -75,6 +92,11 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
             idx_long = idx_short
             short_doc = switch_data
             idx_short = switch_idx
+            
+            if focused_doc == 'after':
+                focused_doc = 'before'
+            else:
+                focused_doc = 'after'
 
         # Grab our text to compare
         text_chunk_short = short_doc[idx_short:idx_short + view_size] # Get chunk ... if idx = 0 and view = 1, get 0
@@ -99,12 +121,13 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
                     idx_short+=view_size
                     idx_long+=view_size
                 else:
-                    comparison_collector.append_change_chunk(text_chunk_long)
+                    comparison_collector.append_change_chunk({'text':text_chunk_long, 'source':focused_doc})
+                    comparison_collector.append_change_text(text_chunk_long)
                     idx_long+=view_size
-                    text_chunk_long = long_doc[idx_long:idx_long + view_size] # update our 'B' text chunk
+                    text_chunk_long = long_doc[idx_long:idx_long + view_size] # update our 'After' text chunk
 
     # Show results
     comparison_collector.print_collected_data()
 
 # Run
-get_diff(doc_before_both, doc_after_both, 1, 3)
+get_diff(doc_before_big, doc_after_big, 1, 3)
