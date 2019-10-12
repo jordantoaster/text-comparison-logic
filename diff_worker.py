a6b5c5244d29d442ce2dab64822401cb08f46234
jordantoaster <jordanmcdonaldmain@gmail.com>
2019-10-12 14:59:31 +0100

    temp

--- diff_worker.py
+++ diff_worker.py
@@ -2,29 +2,32 @@
 
 # 1 Matching Text
 doc_before_match = 'this is a matching string, it is a simple example'
-doc_b_match = 'this is a matching string, it is a simple example'
+doc_after_match = 'this is a matching string, it is a simple example'
 
 # 2 Addition Mid
 doc_before_addition = 'this is a matching string -  - it is a simple example'
-doc_b_addition = 'this is a matching string - new text inserted here - it is a simple example'
+doc_after_addition = 'this is a matching string - new text inserted here - it is a simple example'
 
 # 3 Deletion Mid
 doc_before_deletion = 'this is a matching string - old text deleted here - it is a simple example'
-doc_b_deletion = 'this is a matching string -  - it is a simple example'
+doc_after_deletion = 'this is a matching string -  - it is a simple example'
+
+# 4 Deletion & Addition
+doc_before_both = 'this is a *delete* piece of sample text'
+doc_after_both = 'this is a piece of sample text *add*'
 
 # Data Structure for Changes
 class ChChChChanges():
 
     def __init__(self):     
         self.matching_text = ""
-        self.changed_text = ["",""]
+        self.changed_text = []
 
     def append_matching_chunk(self, text_chunk :str):
         self.matching_text += text_chunk
 
-    def append_change_chunk(self, text_chunk_a :str, text_chunk_b :str):
-        self.changed_text[0] += text_chunk_a
-        self.changed_text[1] += text_chunk_b
+    def append_change_chunk(self, text_chunk :str):
+        self.changed_text.append(text_chunk)
 
     def print_collected_data(self):
         print(self.matching_text)
@@ -36,7 +39,7 @@ class ChChChChanges():
 # TODO change to return the leftover text of the longest string as well.
 def get_shortest_string_len(doc_before :str, doc_after :str):
 
-    if len(doc_before) > len(doc_after):
+    if len(doc_before) >= len(doc_after):
         return len(doc_before), True
     else:
         return len(doc_after), False
@@ -58,6 +61,20 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
         short_doc = doc_before 
 
     while(idx_short < longest_length or idx_long < longest_length):
+
+        # idea to check and flip 'short' and 'long' based on however how the most string left?
+        # Important given logic after this assumes we keep chipping into the longest
+        if (len(long_doc) - idx_long) < (len(short_doc) - idx_short):
+
+            # get temp data for switch
+            switch_data = long_doc
+            switch_idx = idx_long
+            
+            # switch the data, switch the indexes
+            long_doc = short_doc
+            idx_long = idx_short
+            short_doc = switch_data
+            idx_short = switch_idx
 
         # Grab our text to compare
         text_chunk_short = short_doc[idx_short:idx_short + view_size] # Get chunk ... if idx = 0 and view = 1, get 0
@@ -82,7 +99,7 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
                     idx_short+=view_size
                     idx_long+=view_size
                 else:
-                    comparison_collector.append_change_chunk("", text_chunk_long)
+                    comparison_collector.append_change_chunk(text_chunk_long)
                     idx_long+=view_size
                     text_chunk_long = long_doc[idx_long:idx_long + view_size] # update our 'B' text chunk
 
@@ -90,4 +107,4 @@ def get_diff(doc_before :str, doc_after :str, view_size :int, retrieval_buffer :
     comparison_collector.print_collected_data()
 
 # Run
-get_diff(doc_before_addition, doc_b_addition, 1, 3)
+get_diff(doc_before_both, doc_after_both, 1, 3)
